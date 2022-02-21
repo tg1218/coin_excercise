d0384f2cfe4fbaeaad2de320a1e11db48dc278a5
tg1218 <tg1218.kim@gmail.com>
2022-02-21 16:02:52 +0900

    추가 - 완료 (1) 유저의 기본 정보 조회 기능 (2) from, to로 이더 보내는 기능 추가

--- domain/account.js
+++ domain/account.js
@@ -2,12 +2,31 @@ const {User} = require('../src/db/models');
 
 module.exports = {
     getAccountInfo: async(req, res, next) => {
-        let user = await User.findAll({
-            attributes=["address", "balance"],
-            where:{ address: req.body.address }});
-        
-        let {address, balance} = user;
-        
+        let {address} = req.body;
+
+        if(address){
+            let user = await User.findAll({
+                where:{ address: req.body.address }});
+            
+            if(user.length > 0){
+                let {address, balance} = user[0];
+    
+                res.status(200).send({
+                    address: address,
+                    balance: balance
+                });
+            }
+            else{
+                res.status(500).send({
+                    message: "There is no account"
+                });
+            }
+        }
+        else{
+            res.status(200).send({
+                message: "Login Please"
+            });
+        }
     },
     makeAccount: async(req, res, next) => {
         const password = req.body.password;
@@ -24,6 +43,20 @@ module.exports = {
             data: {
                 address: newAccAddr
             }
+        });
+    },
+    transferEth: async(req, res, next) => {
+        const { from_address, to_address, value } = req.body;
+
+        web3.eth.sendTransaction({
+            from: from_address,
+            to: to_address,
+            value: web3.utils.toWei(value, 'ether')
+        }).on('receipt', (receipt)=>{
+            res.status(200).send({
+                message:'Sucess',
+                data:{receipt}
+            });
         });
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const {getAccountInfo, makeAccount} = require('../domain/account');
+const {getAccountInfo, makeAccount, transferEth} = require('../domain/account');
 
 router.get('/', getAccountInfo);
 
 router.post('/new', makeAccount);
+
+router.post('/transfer', transferEth);
 
 module.exports = router;
